f2dae6e0bb9fbafa9f410424eaf227f91fc0f379
Mendi Lash <Mendi Lash>
2016-09-23 20:44:37 -0500

    first commit

---
+++ src/test/java/edu/jsu/mcis/ConverterTest.java
@@ -0,0 +1,56 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ConverterTest {
+    private String csvString;
+    private String jsonString;
+
+
+    private static String readFile(String path) throws IOException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        String newline = System.getProperty("line.separator");
+        String out = "";
+
+        try {
+            while(scanner.hasNextLine()) {
+                out += scanner.nextLine() + newline;
+            }
+            return out;
+        } finally {
+            scanner.close();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        try {
+			
+			csvString = readFile("src/test/resources/grades.csv");
+            jsonString = readFile("src/test/resources/grades.json");
+            
+        } catch(IOException e) {}
+    }
+
+    @Test
+    public void testConvertCSVtoJSON() {
+        assertTrue(Converter.jsonStringsAreEqual(Converter.csvToJson(csvString), jsonString));
+    }
+
+    @Test
+    public void testConvertJSONtoCSV() {
+        assertEquals(Converter.jsonToCsv(jsonString), csvString);
+    }
+}
+
+
+
+
+
